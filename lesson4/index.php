7dc94ac50b9047dc9b98a3e0f8cbd386e2b7a95d
Dreni956 <drendren174@gmail.com>
2025-04-01 16:06:00 +0200

    lesson4

--- lesson4/index.php
+++ lesson4/index.php
@@ -3,8 +3,40 @@
 // $x="Hello";
 // print_r($x);
 
-   $var = 23.4;
-   echo gettype($var) . "<br>";
+   // $var = 23.4;
+   // echo gettype($var) . "<br>";
+?>
+<?php
+// $sports = array('Real Madridi','Benfica', 'Barcelona');
+// array_unshift($sports, 'Chelsea');
 
+// var_dump($sports);
+?>
+
+<?php 
+   //   $sports = array('Real Madridi','Benfica', 'Barcelona');
+   //   array_shift($sports);
+   //   var_dump($sports);
+?>
+
+<?php 
+   //  $sports = ['Real Madridi','Benfica', 'Barcelona', 'Bayern', 'Arsenal'];
+   //  $output = array_slice($sports , 2);
+   //  $output1 = array_slice($sports , 3);
+   //  $output2 = array_slice($sports , -2 , 1);
+
+   //  var_dump($output , $output1 , $output2);
+
+?>
+
+<?php 
+// $sports = array(12,24,36,48);
+// var_dump(array_sum($sports));
+?>
+
+<?php
+$temp = [25,30,25,26,20,28,25];
+$averagetemp = (array_sum ($temp)/7);
+echo($averagetemp);
 
 ?>
