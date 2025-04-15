ae676818dafe5fc467bf35ce4b2ec54f88efdaaf
Dreni956 <drendren174@gmail.com>
2025-04-15 16:03:32 +0200

    changes

--- lesson5/index.php
+++ lesson5/index.php
@@ -17,3 +17,52 @@ for($row=0; $row<3; $row++){
    echo "</ul>";
 }
 ?>
+
+<?php 
+// $array = array(
+//    array(1,2,3)
+//    array(1,2,3)
+//    array(1,2,3)
+// );
+
+// for($i=0,$i<4, $i++){
+//    for ($j=0, $j<4 , $j++){
+//       echo "Array $i of $j";
+//    }
+// }
+
+
+
+
+//  for($i=0,$i <5, $i++){
+//    for($j=$i, $j <=$i , $j++){
+//       echo "&nbsp";
+//   }
+//    for($j=1, $j <=$i , $j++){
+//        echo "*";
+//    }
+//    echo "<br>";
+// }
+?>
+
+<?php
+$grade = array(
+   "Math" => 4,
+   "Art" => 3,
+   "History" => 2,
+   "Music" => 3,
+)
+
+// echo "Math grade is:".$grade['Math'];
+foreach($grade as $subject => $grade){
+   echo "Subject:$subject Grade:$grade <br>"
+}
+
+
+
+?>
+
+
+
+
+
