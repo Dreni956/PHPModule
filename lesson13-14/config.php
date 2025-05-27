651c905a65f74b348214455dcadd58a58c85b4c3
Dreni956 <drendren174@gmail.com>
2025-05-27 16:20:19 +0200

    day3

--- lesson13-14/config.php
+++ lesson13-14/config.php
@@ -1,7 +1,4 @@
-
-
 <?php
-session_start();
 
 $user="root";
 $pass="";
@@ -12,7 +9,12 @@ try {
 	
 	$conn =new PDO("mysql:host=$server;dbname=$dbname",$user,$pass);
 
+
 } catch (PDOException $e) {
 	echo "error: " . $e->getMessage();
 }
+
+
+
+
  ?>
